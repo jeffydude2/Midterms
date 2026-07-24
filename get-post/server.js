8d47ddb8d49ddf245942114163dce41e64a1c65d
jeffydude2 <jeffdabawse@gmail.com>
2026-07-24 16:15:55 +0800

    Add files via upload

---
+++ get-post/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/process-get', (req, res) => {
+  const { first_name, last_name } = req.query;
+
+  if (!first_name || !last_name) {
+    return res.status(400).json({
+      error: 'Both first_name and last_name are required.'
+    });
+  }
+
+  res.json({
+    first_name,
+    last_name
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+//post route
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/process-post', (req, res) => {
+  const { first_name, last_name } = req.body;
+
+  if (!first_name || !last_name) {
+    return res.status(400).json({
+      error: 'Both first_name and last_name are required.'
+    });
+  }
+
+  res.status(200).json({
+    message: 'Form submitted successfully.',
+    first_name,
+    last_name
+  });
+});
