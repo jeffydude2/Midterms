d3dcacf7950e545a4824bef8f0fe5459327ebb7e
jeffydude2 <jeffdabawse@gmail.com>
2026-07-24 14:53:24 +0800

    Add files via upload

---
+++ week_7/server_routing.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+ 
+// responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+  console.log("There is GET request for the homepage!");
+  res.send('Here is the GET Method!');
+})
+ 
+// responds to a POST request for the homepage
+app.post('/', function (req, res) {
+  console.log("A POST request for the homepage is accessed.");
+  res.send('Here is the POST Method');
+})
+ 
+// responds to a GET request for /list_user
+app.get('/list_user', function (req, res) {
+  console.log("Got a GET request for /list_user");
+  res.send('This is Page Listing');
+})
+
+// NEW ROUTE: responds to a GET request for /jefferson
+app.get('/jefferson', function (req, res) {
+  console.log("Got a GET request for /jefferson");
+  res.send('Hello my name is Jefferson Tan from WD-301');
+})
+ 
+// matches abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {
+  console.log("Got a GET request for /ab*cd");
+  res.send('Pattern Match Page');
+})
+ 
+var server = app.listen(4000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+})
